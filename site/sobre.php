85cd415df9cc798411179c17502a058f806e8da2
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2019-05-31 12:21:08 -0300

    inclusão do maps

--- site/sobre.php
+++ site/sobre.php
@@ -22,6 +22,14 @@ session_start();
   </p>
 	</div>
 
+  <div class="container" id="mapa">
+    <h2>Localização da empresa</h2>
+    <br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.2373891873226!2d127.79104731437094!3d-19.17298498703232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTnCsDEwJzIyLjgiUyAxMjfCsDQ3JzM1LjciRQ!5e1!3m2!1spt-BR!2sbr!4v1559315974727!5m2!1spt-BR!2sbr"
+     width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+    </iframe>
+
+  </div>
 
 </div>
 <?php include("includes/footer.php"); ?>
