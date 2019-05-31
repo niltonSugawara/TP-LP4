fdaa478fcfb438f0d3615cfff174c4254e4de324
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2019-05-31 12:00:20 -0300

    Corrigindo algumas alterações

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -4,22 +4,5 @@
 	</a>
 </div>
 
-<div class="container relative">
-	<a id="rodape">
-			©2019 Grupo NP | FATEC PG - ADS NOTURNO
-	</a>
-</div>
-
-<div class="container relative">
-	<a id="rodape">
-			©2019 Grupo NP | FATEC PG - ADS NOTURNO
-	</a>
-</div>
-
-<div class="container relative">
-	<a id="rodape">
-			©2019 Grupo NP | FATEC PG - ADS NOTURNO
-	</a>
-</div>
 <script type="text/javascript" src="../resources/js/jquery-3.4.1.min.js"></script>
 <script type="text/javascript" src="../resources/js/bootstrap/bootstrap.bundle.min.js"></script>
--- site/login.php
+++ site/login.php
@@ -1,5 +1,5 @@
 <?php
-session_start()
+session_start();
 $conexao = mysqli_connect("localhost","root","","museuGames");
 $usuario = $_POST("usuario");
 $senha = $_POST("senha");
